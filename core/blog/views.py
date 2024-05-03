14f401e938d7b47a7ad22b55914ac9f922ce7354
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-05-03 07:34:31 -0400

    + pagination to post list

--- core/blog/views.py
+++ core/blog/views.py
@@ -26,6 +26,7 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "post_list"
+    paginate_by = 9
 
     def get_queryset(self) -> QuerySet[Any]:
         posts = Post.objects.filter(status=True)
